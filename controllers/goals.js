0df4a09b351bc617ad526ecaef02563c97de09d4
jaymark <jaykmark@gmail.com>
2019-12-08 19:00:10 -0800

    populates skill on created/updated goals.

--- controllers/goals.js
+++ controllers/goals.js
@@ -4,17 +4,14 @@ const db = require('../models');
 // POST - Create Goal
 const create = async (req, res) => {
   try {
-    const createdGoal = await db.Goal.create(req.body)
+    const createdGoal = await db.Goal.create(req.body).populate('skill');
     // Find Skill By ID and Save
-    const foundSkill = await db.Skill.findById(req.body.skill)
-    foundSkill.goals = createdGoal._id
-    foundSkill.save()
-    res.status(200).json({
+    const foundSkill = await db.Skill.findById(req.body.skill);
+    foundSkill.goals = createdGoal._id;
+    foundSkill.save();
+    res.status(200).populate('skill').json({
       status: 200,
-      data: createdGoal.populate({
-        path: 'skill',
-        model: 'Skill',
-      }),
+      data: createdGoal,
     })
   } catch (err) {
     return res.status(500).json({
@@ -42,7 +39,7 @@ const index = async (req, res) => {
 // GET - Show Goal by ID
 const show = async (req, res) => {
   try {
-    const foundGoal = await db.Goal.findById(req.params.id);
+    const foundGoal = await db.Goal.findById(req.params.id).populate('skill');
     res.status(200).json({ 
       status: 200,
       data: foundGoal,
@@ -57,7 +54,7 @@ const show = async (req, res) => {
 // PUT - Update Goal by ID
 const update = async (req, res) => {
   try {
-    const updatedGoal = await db.Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedGoal = await db.Goal.findByIdAndUpdate(req.params.id, req.body, { new: true }).populate('skill');
     res.status(200).json({
       status: 200,
       data: updatedGoal,
